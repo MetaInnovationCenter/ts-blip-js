5bf90b2d5dcaaa982ab2142ac96596f02857fcd0
Emanuel de Souza Scherer <emanuel.scherer@meta.com.br>
2020-01-29 15:21:07 -0300

    Mensagem Options

--- emf.js
+++ emf.js
@@ -1,5 +1,6 @@
 module.exports = {
 
+    //MandaMensagem bunita (var client, var idUser, Mensagem, Tempo de Digitando em segundos (0 = sem digitando))
     SendMessage: (client, user, msg, dg) => {
 
         if (dg > 0) { 
@@ -37,7 +38,28 @@ module.exports = {
         }
     },
 
+    //MandaMensagem com Opções (var client, var idUser, Mensagem, ["Opção1","Opção2","Opção3","Opção4"] (de 1 a 4 opções)) 
     SendOptions: (client, user, msg, ops) => {
+
+        if (ops.length == 1) {
+
+            client.sendMessage({
+
+                type: "application/vnd.lime.select+json",
+                to: user,
+                content: {
+    
+                    scope:"immediate",
+                    text: msg,
+        
+                    options: [
+                        {
+                            text: ops[0]
+                        }
+                    ]
+                }
+            })
+        }
 
         if (ops.length == 2) {
 
