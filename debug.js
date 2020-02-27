42eb5819d9b2795122b297101a4c9a044717a33e
Nicolas <nicolas.araujo@meta.com.br>
2020-02-27 16:56:39 -0300

    added bayes classifier on debug

--- debug.js
+++ debug.js
@@ -1,9 +1,106 @@
-let str = '500 erro 82ue555jihf902 524 je09djwaposd kao koad 0w 500'
-let str2 = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-let regex = /\b(5)(\d{2})\b/i
+var bayes = require('bayes')
 
-console.log(str2.match(regex))
+// let jsonTest = {
+//     cat1 : [1, 2, 3],
+//     cat2 : [2, 3, 4]
+// }
 
-if(str2.match(regex) == null) {console.log('tudo certo')}
+// for (item in jsonTest) {
+//     console.log(jsonTest[item])
+// }
 
-if(str.match(regex) != null) {console.log('erro')}
+// /**
+//  * Learn multiple phrases from a json
+//  * @param {String} defaultCategory Called when no other category is matched
+//  * @param {Array} phrases Phrases to be learned
+//  * @param {String} category Category to train in
+//  */
+// multilearn = async (defaultCategory, category, phrases) => {
+//     await classifier.learn('', defaultCategory)
+
+//     phrases.forEach(phrase => {
+//         classifier.learn(phrase, category)
+//     });
+// }
+
+/**
+ * Classifies an input phrase based on the categories and training phrases provided in a JavaScript Object format
+ * @param {JSON} catsAndPhrases JavaScript Object containing the categories and phrases that will be classified  
+ * @param {String} message The message that needs to be classified
+ */
+const classifyMessage = async (message, catsAndPhrases) => {
+    let classifier = bayes()
+
+    //Default category activated when no other is matched
+    await classifier.learn('', 'No Category Found')
+
+    //Learning to match all the phrases with their respective categories//
+    //For each category
+    for (category in catsAndPhrases) {
+        let phrases = catsAndPhrases[category]
+
+        //For each phrase in a category: learn it
+        phrases.forEach(async phrase => {
+            await classifier.learn(phrase, category)
+        });
+    }
+
+    //Categorize input message
+    return classifier.categorize(message)
+}
+
+app = async () => {
+
+    classifyMessage('Ok', {Certo: ['Beleza', 'Tranquilo', 'Ok']})
+    .then((response) => {
+        console.log(response)
+    })
+
+    // multilearn(
+    //     'Não entendi',
+    //     'Reset de Senha',
+    //     [
+    //         'trocar minha senha',
+    //         'resetar minha senha',
+    //         'senha nova',
+    //         'esqueci',
+    //         'não consigo abrir'
+    //     ])
+    // await classifier.categorize('joão')
+    // .then((response) => {
+    //     console.log(response)
+    // })
+
+    // multilearn(
+    //     'Não entendi',
+    //     'Sim',
+    //     [
+    //         'sim',
+    //         'certo',
+    //         'ok',
+    //         'beleza'
+    //     ])
+    // await classifier.categorize('certo')
+    // .then((response) => {
+    //     console.log(response)
+    // })
+}
+
+app()
+
+
+
+
+
+
+
+
+// let str = '500 erro 82ue555jihf902 524 je09djwaposd kao koad 0w 500'
+// let str2 = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+// let regex = /\b(5)(\d{2})\b/i
+
+// console.log(str2.match(regex))
+
+// if(str2.match(regex) == null) {console.log('tudo certo')}
+
+// if(str.match(regex) != null) {console.log('erro')}
